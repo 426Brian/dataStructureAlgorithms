f3acdc43e8478b7643cc28c3d89b779053c732bf
426Brian <1165380405@qq.com>
2020-01-07 21:29:14 +0800

    search algorithm

--- DataStructure/src/search/BinarySearch.java
+++ DataStructure/src/search/BinarySearch.java
@@ -2,7 +2,13 @@ package search;
 
 public class BinarySearch {
     public static void main(String[] args) {
+        int[] arr = {1, 8, 10, 89,1000, 1234};
 
+        int result = binarySearch(arr, 0, arr.length, 89);
+
+        int result2 = binarySearch2(arr, 89);
+
+        System.out.println("result = "+result+" result2 = "+result2);
     }
 
     /**
@@ -14,7 +20,7 @@ public class BinarySearch {
      */
     public static int binarySearch(int[] arr, int left, int right, int findVal) {
         // 没有找到
-        if(left > right){
+        if (left > right) {
             return -1;
         }
 
@@ -27,9 +33,29 @@ public class BinarySearch {
         } else if (findVal < midVal) {
             right = mid - 1;
             binarySearch(arr, left, right, findVal);
-        }else {
+        } else {
             return mid;
         }
         return 0;
+    }
+
+    public static int binarySearch2(int[] arr, int findVal) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            int midVal = arr[mid];
+
+            if (findVal > midVal) {
+                low = mid + 1;
+            } else if (findVal < midVal) {
+                low = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
     }
 }
--- DataStructure/src/search/SeqSearch.java
+++ DataStructure/src/search/SeqSearch.java
@@ -5,22 +5,23 @@ public class SeqSearch {
         int[] arr = {1, 9, 11, -1, 34, 89};
 
         int index = seqSearch(arr, 11);
-        if(index == -1){
+        if (index == -1) {
             System.out.println("没有找到");
-        }else {
-            System.out.println("找到， 下标为："+index);
+        } else {
+            System.out.println("找到， 下标为：" + index);
         }
     }
 
     /**
-     *  现行查找，找到一个满足条件的值即可
+     * 现行查找，找到一个满足条件的值即可
+     *
      * @param arr
      * @param value
      * @return
      */
-    public static int seqSearch(int[] arr, int value){
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] ==value){
+    public static int seqSearch(int[] arr, int value) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == value) {
                 return i;
             }
         }
